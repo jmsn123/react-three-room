a701dad4b74736ccba82149d8337cf93a0a3d019
rashawn doyley <rashawndoyley12@gmail.com>
2024-12-23 07:13:20 -0500

    made vite config edits deployment isnt working

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Adjust if your app is hosted in a subdirectory
+  // base: "/", // Set this if your app is hosted in a subdirectory
   build: {
-    outDir: "dist" // Ensure the output directory is "dist"
+    outDir: "dist" // Ensure the build output is in the "dist" folder
   }
 });
